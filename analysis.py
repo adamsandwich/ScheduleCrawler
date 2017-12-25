377e0789cd8345a34bac736940ee2f8574b97d1e
Adam sandwich <Adamsandwich@outlook.com>
2017-12-25 19:46:08 +0800

    新增：正则匹配课程基本信息

--- analysis.py
+++ analysis.py
@@ -1,15 +1,25 @@
-import os,sys
 import re
+
 
 def analysis():
     file_object = open("C:\\Users\\Adams\\PycharmProjects\\ScheduleCrawler\\testdata.txt", encoding='utf-8')
     try:
         all_the_text = file_object.read()
-        courseinfo=re.compile("activity = new TaskActivity(.*);")
-        print(courseinfo.findall(all_the_text))
+        courseinfo = re.compile("activity = new TaskActivity(.*);")
+        # 5:CourseName 7:CourseClassroom 8:ValidWeek
+        for each in courseinfo.findall(all_the_text):
+            course = each.split(",")
+            print(course[5].replace("\"", ""))
+            print(course[7].replace("\"", ""))
+            print(course[8].replace("\"", ""))
+            break
     finally:
         file_object.close()
 
 
-if __name__  == '__main__':
+''' 正则
+activity = new TaskActivity(.*);.*var teachers
+'''
+
+if __name__ == '__main__':
     analysis()
