7947f41114e0e4f6037e29abfc7d271cbad5cbb9
Adam sandwich <Adamsandwich@outlook.com>
2017-12-25 19:08:21 +0800

    修改：测试数据源

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import os,sys
 import re
 
 def analysis():
-    file_object = open("C:\\Users\\Adams\\Desktop\\z.txt", encoding='utf-8')
+    file_object = open("C:\\Users\\Adams\\PycharmProjects\\ScheduleCrawler\\testdata.txt", encoding='utf-8')
     try:
         all_the_text = file_object.read()
         courseinfo=re.compile("activity = new TaskActivity(.*);")
