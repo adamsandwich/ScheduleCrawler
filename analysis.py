76017f298e22b598c3ce9f42ee8ed9c59c39c8e5
Adam sandwich <Adamsandwich@outlook.com>
2017-12-26 14:24:29 +0800

    新增：获取课程基本信息

--- analysis.py
+++ analysis.py
@@ -4,22 +4,32 @@ import re
 def analysis():
     file_object = open("C:\\Users\\Adams\\PycharmProjects\\ScheduleCrawler\\testdata.txt", encoding='utf-8')
     try:
+        list = []
         all_the_text = file_object.read()
-        courseinfo = re.compile("activity = new TaskActivity(.*);")
-        # 5:CourseName 7:CourseClassroom 8:ValidWeek
-        for each in courseinfo.findall(all_the_text):
-            course = each.split(",")
-            print(course[5].replace("\"", ""))
-            print(course[7].replace("\"", ""))
-            print(course[8].replace("\"", ""))
-            break
+        # 获取课程主体文本
+        processed_text = re.compile(r"var activity=null;.*table0.marshalTable", re.M|re.S).findall(all_the_text)[0]
+        # 处理文本
+        for each in processed_text.split("var teachers"):
+            dic = {}
+            # 获取教师姓名
+            teacherInfo = re.findall(r"var actTeachers .{0,62}", each)
+            if len(teacherInfo):
+                teacherName = re.findall(r"name:\"(.{0,8})\"", teacherInfo[0])
+                if len(teacherName):
+                    dic['teacherName'] = teacherName[0]
+            # 获取课程 名称、地点、周次
+            for eachinfo in re.findall(r"activity = new TaskActivity(.{0,220});", each):
+                info=eachinfo.split(",")
+                courseName = info[5].replace("\"", "")
+                courseClassroom = info[7].replace("\"", "")
+                courseWeek =info[8].replace("\"", "")
+                dic['courseName'] =courseName
+                dic['courseClassroom'] = courseClassroom
+                dic['courseWeek'] = courseWeek
+                list.append(dic)
     finally:
         file_object.close()
 
-
-''' 正则
-activity = new TaskActivity(.*);.*var teachers
-'''
 
 if __name__ == '__main__':
     analysis()
